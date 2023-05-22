cbae24ad5d1adce72b52d4a0ba75383a6ce974f0
Kate Khymochka <kkhymochka@my.bcit.ca>
2023-05-22 14:26:52 -0700

    front page tasks done

--- front-page.php
+++ front-page.php
@@ -23,13 +23,47 @@ get_header();
 
 			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
 		endwhile; // End of the loop.
 		?>
+
+		<!-- Add 3 recent news -->
+		<section class = "home-blog">
+			<h2>
+				<?php 
+				 esc_html_e('Recent News', 'fwd')
+		 		?>
+			</h2>
+			
+			<?php
+			$args = array( 
+				'post_type'      => 'post',
+				'posts_per_page' => 3 
+			);
+
+			$blog_query = new WP_Query( $args );
+
+			if ( $blog_query -> have_posts() ) {
+				while ( $blog_query -> have_posts() ) {
+					$blog_query -> the_post();
+					?>
+					<article>
+						<a href="<?php the_permalink(); ?>">
+							<h3><?php the_title(); ?></h3>
+							<?php 
+							the_post_thumbnail( 'medium' );?>
+							
+						</a>
+					</article>
+					<?php
+				}
+				wp_reset_postdata();
+			}
+			?>
+
+			
+				
+			
+		</section>
 
 	</main><!-- #main -->
 
